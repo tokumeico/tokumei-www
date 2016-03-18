a5e4f55991b924ba69637230da69d15ba6194604
Kyle Farwell <m@kfarwell.org>
2016-03-18 15:35:46 -0400

    initial commit of 0.2.1

---
+++ js/init.js
@@ -0,0 +1,8 @@
+(function($){
+  $(function(){
+    $('.parallax').parallax();
+    $('.slider').slider({full_width: true, height: 200, indicators: false});
+    $('.button-collapse').sideNav();
+    $('.materialboxed').materialbox();
+  });
+})(jQuery);
